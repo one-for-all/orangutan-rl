0aabfebf8a76c8731977f1fdd095ba35713e617b
Jay Li <jn.sapiens@gmail.com>
2026-05-20 22:02:38 +0900

    feat: try burn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use burn::{Tensor, prelude::Backend};
+
+fn computation<B: Backend>() {
+    let device = Default::default();
+    let tensor1: Tensor<B, 2> = Tensor::from_floats([[2., 3.], [4., 5.]], &device);
+    let tensor2 = Tensor::ones_like(&tensor1);
+
+    println!("{:}", tensor1 + tensor2);
+}
+
 fn main() {
-    println!("Hello, world!");
+    computation::<burn::backend::Wgpu>();
 }
