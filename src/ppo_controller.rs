89a8721a12ec5a8eda10bbd59921f484b6883386
Jay Li <jn.sapiens@gmail.com>
2026-06-04 21:12:27 +0900

    feat: make number of steps taken per control request adjustable

--- src/ppo_controller.rs
+++ src/ppo_controller.rs
@@ -60,6 +60,12 @@ pub struct PPOPendulumController {
 
     k: usize,
     last_act: f64,
+
+    o: Vec<f32>,
+    ep_ret: f32,
+    ep_len: usize,
+    init_q: f32,
+    t: usize,
 }
 
 impl PPOPendulumController {
@@ -70,7 +76,7 @@ impl PPOPendulumController {
         let rng = StdRng::seed_from_u64(1);
 
         let dt = 1. / 60. * CONTROL_HZ_DIVIDER as f32;
-        let env = PendulumEnv::new(dt);
+        let mut env = PendulumEnv::new(dt);
 
         let obs_dim = env.obs_dim();
         let act_dim = env.act_dim();
@@ -85,7 +91,13 @@ impl PPOPendulumController {
         let pi_optimizer = AdamConfig::new().init();
         let vf_optimizer = AdamConfig::new().init();
 
-        let last_start_top = false;
+        let last_start_top = !SWINGUP;
+
+        // Prepare for interaction with environment
+        let init_q = if SWINGUP { PI } else { 0. };
+        let o = env.reset(init_q);
+        let ep_ret = 0.;
+        let ep_len = 0;
 
         Self {
             ac,
@@ -98,93 +110,106 @@ impl PPOPendulumController {
             last_start_top,
             k: 0,
             last_act: 0.,
+
+            o,
+            ep_ret,
+            ep_len,
+            init_q,
+            t: 0,
+        }
+    }
+}
+
+impl PPOPendulumController {
+    pub fn run_step(&mut self) {
+        let (a, v, logp) = self
+            .ac
+            .step(vec2d_to_tensor(vec![self.o.clone()], &Default::default()));
+
+        let (next_o, r) = self.env.step(a);
+        self.ep_ret += r;
+        self.ep_len += 1;
+
+        // save to buffer
+        self.buf.store(self.o.clone(), a, r, v, logp);
+
+        // Update obs
+        self.o = next_o;
+
+        let timeout = self.ep_len == MAX_EP_LEN;
+        let epoch_ended = self.t == STEPS_PER_EPOCH - 1;
+        if timeout || epoch_ended {
+            if epoch_ended && !timeout {
+                println!(
+                    "Warning: trajectory cut off by epoch at {} steps.",
+                    self.ep_len
+                );
+            }
+            // if trajectory didn't reach terminal state, bootstrap value target
+            let last_v = self
+                .ac
+                .v
+                .forward(vec2d_to_tensor(vec![self.o.clone()], &Default::default()))
+                .into_scalar()
+                .elem();
+            self.buf.finish_path(last_v);
+
+            if timeout {
+                if (self.last_start_top && !SWINGUP) || (!self.last_start_top && SWINGUP) {
+                    console_log(&format!("episode return: {:.5}", self.ep_ret));
+                }
+            }
+
+            self.o = if self.rng.random_bool(0.5) {
+                self.last_start_top = false;
+                self.env.reset(self.init_q) // reset pendulum to bottom
+            } else {
+                self.last_start_top = true;
+                self.env.reset(0.) // reset pendulum to top
+            };
+
+            self.ep_ret = 0.;
+            self.ep_len = 0;
         }
     }
 }
 
 impl ArticulatedController for PPOPendulumController {
     fn control(&mut self, articulated: &Articulated, input: &Vec<Float>) -> DVector<Float> {
-        // Prepare for interaction with environment
-        let init_q = if SWINGUP { PI } else { 0. };
-        let mut o = self.env.reset(init_q);
-        let mut ep_ret = 0.;
-        let mut ep_len = 0;
-
-        for t in 0..STEPS_PER_EPOCH {
-            let (a, v, logp) = self
-                .ac
-                .step(vec2d_to_tensor(vec![o.clone()], &Default::default()));
-
-            let (next_o, r) = self.env.step(a);
-            ep_ret += r;
-            ep_len += 1;
-
-            // save to buffer
-            self.buf.store(o.clone(), a, r, v, logp);
-
-            // Update obs
-            o = next_o;
-
-            let timeout = ep_len == MAX_EP_LEN;
-            let epoch_ended = t == STEPS_PER_EPOCH - 1;
-            if timeout || epoch_ended {
-                if epoch_ended && !timeout {
-                    println!("Warning: trajectory cut off by epoch at {} steps.", ep_len);
-                }
-                // if trajectory didn't reach terminal state, bootstrap value target
-                let last_v = self
-                    .ac
-                    .v
-                    .forward(vec2d_to_tensor(vec![o.clone()], &Default::default()))
-                    .into_scalar()
-                    .elem();
-                self.buf.finish_path(last_v);
-
-                if timeout {
-                    if (self.last_start_top && !SWINGUP) || (!self.last_start_top && SWINGUP) {
-                        console_log(&format!("episode return: {:.5}", ep_ret));
-                    }
-                }
-
-                o = if self.rng.random_bool(0.5) {
-                    self.last_start_top = false;
-                    self.env.reset(PI) // reset pendulum to bottom
-                } else {
-                    self.last_start_top = true;
-                    self.env.reset(0.) // reset pendulum to top
-                };
-
-                ep_ret = 0.;
-                ep_len = 0;
-            }
+        for _ in 0..MAX_EP_LEN {
+            self.run_step();
+            self.t += 1;
         }
 
-        // Perform VPG update
-        self.buf.get();
+        if self.t >= STEPS_PER_EPOCH {
+            self.t = 0;
+            // Perform VPG update
+            self.buf.get();
 
-        // Train policy with a single step of gradient descent
-        for i in 0..TRAIN_PI_ITERS {
-            let (loss_pi, kl) = compute_loss_pi_ppo(&self.buf, &self.ac.pi, CLIP_RATIO);
-            if kl > 1.5 * TARGET_KL {
-                println!("Early stopping at step {} due to reaching max kl", i);
-                break;
+            // Train policy with a single step of gradient descent
+            for i in 0..TRAIN_PI_ITERS {
+                let (loss_pi, kl) = compute_loss_pi_ppo(&self.buf, &self.ac.pi, CLIP_RATIO);
+                if kl > 1.5 * TARGET_KL {
+                    println!("Early stopping at step {} due to reaching max kl", i);
+                    break;
+                }
+                let gradients = loss_pi.backward();
+                let gradient_params = GradientsParams::from_grads(gradients, &self.ac.pi);
+
+                // TODO: avoid the clone
+                self.ac.pi = self
+                    .pi_optimizer
+                    .step(PI_LR, self.ac.pi.clone(), gradient_params);
             }
-            let gradients = loss_pi.backward();
-            let gradient_params = GradientsParams::from_grads(gradients, &self.ac.pi);
 
-            // TODO: avoid the clone
-            self.ac.pi = self
-                .pi_optimizer
-                .step(PI_LR, self.ac.pi.clone(), gradient_params);
-        }
-
-        for _ in 0..TRAIN_V_ITERS {
-            let loss_v = compute_loss_v(&self.buf, &self.ac.v);
-            let gradients = loss_v.backward();
-            let gradient_params = GradientsParams::from_grads(gradients, &self.ac.v);
-            self.ac.v = self
-                .vf_optimizer
-                .step(VF_LR, self.ac.v.clone(), gradient_params);
+            for _ in 0..TRAIN_V_ITERS {
+                let loss_v = compute_loss_v(&self.buf, &self.ac.v);
+                let gradients = loss_v.backward();
+                let gradient_params = GradientsParams::from_grads(gradients, &self.ac.v);
+                self.ac.v = self
+                    .vf_optimizer
+                    .step(VF_LR, self.ac.v.clone(), gradient_params);
+            }
         }
 
         self.num_epochs += 1;
